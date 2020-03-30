6b538ac71afcdc5b5b12d6cbd13656888c09588e
Oskar <oska1204@edu.campusvejle.dk>
2020-03-30 17:19:50 +0200

    Still testing

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', e => {
     // Get elements
+    const btnLoginMenu = document.getElementById('btnLoginMenu')
+    const btnAddLinkMenu = document.getElementById('btnAddLinkMenu')
     const txtEmail = document.getElementById('txtEmail')
     const txtPassword = document.getElementById('txtPassword')
     const btnLogin = document.getElementById('btnLogin')
     const btnSignUp = document.getElementById('btnSignUp')
     const btnLogout = document.getElementById('btnLogout')
+    const btnAddLink = document.getElementById('btnAddLink')
     // Add login event
     btnLogin.addEventListener('click', e => {
         // Get email and pass
@@ -13,7 +16,8 @@ document.addEventListener('DOMContentLoaded', e => {
         const auth = firebase.auth()
         // Sign in
         const promise = auth.signInWithEmailAndPassword(email, pass)
-        promise.catch(console.log)
+        promise
+            .catch(alert)
     })
     // Create User event
     btnSignUp.addEventListener('click', e => {
@@ -25,7 +29,7 @@ document.addEventListener('DOMContentLoaded', e => {
         // Sign in
         const promise = auth.createUserWithEmailAndPassword(email, pass)
         promise
-            .catch(console.log)
+            .catch(alert)
     })
     btnLogout.addEventListener('click', e => {
         firebase.auth().signOut()
@@ -34,10 +38,36 @@ document.addEventListener('DOMContentLoaded', e => {
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
             console.log(firebaseUser)
+            btnAddLinkMenu.classList.remove('hide')
             btnLogout.classList.remove('hide')
+            document.querySelector('.login').classList.add('hide')
         } else {
             console.log('not logged in')
+            btnAddLinkMenu.classList.add('hide')
             btnLogout.classList.add('hide')
+            document.querySelector('.login').classList.add('hide')
         }
     })
+
+    const ulList = document.getElementById('list')
+    const dbRefObject = firebase.database().ref().child('lists')
+    console.log(dbRefObject)
+    const dbRefList = dbRefObject.child('list')
+    dbRefList.on('child_added', snap => {
+        const li = document.createElement('li')
+        li.innerHTML = format(snap.val())
+        li.id = snap.key
+        ulList.appendChild(li)
+    })
+    dbRefList.on('child_changed', snap => {
+        const liChanged = document.getElementById(snap.key)
+        liChanged.innerHTML = format(snap.val())
+    })
+    dbRefList.on('child_removed', snap => {
+        const liToRemove = document.getElementById(snap.key)
+        liToRemove.parentNode.removeChild(liToRemove)
+    })
+    function format(link) {
+        return `<a href="${link}" target="_blank">${link}</a>`
+    }
 })
